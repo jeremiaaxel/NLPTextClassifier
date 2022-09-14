e12fda03a1bec987db39af4132706a0aec7d4217
Jeremia Axel <jeremiaaxelbachtera@gmail.com>
2022-09-14 20:35:08 +0700

    deeplearning using fasttext

---
+++ deeplearning.py
@@ -0,0 +1,73 @@
+# %% Import Libraries
+import os
+import csv
+
+import pandas as pd
+import numpy as np
+
+import fasttext
+from gensim.utils import simple_preprocess
+
+# %% Configurations
+
+TRAIN_FILENAME = "train.csv"
+TEST_FILENAME = "test.csv"
+LABEL_FILENAME = "labels.txt"
+INPUT_FOLDER_PATH = "data_worthcheck"
+
+FASTTEXT_TRAIN_FILENAME = "train.txt"
+FASTTEXT_TEST_FILENAME = "test.txt"
+OUTPUT_FOLDER = "out"
+
+TEST_PREDICT_TEXT = "Josep ganteng anjay"
+
+__BASE_PATH = os.getcwd()
+__FULL_OUTPUT_FOLDER = os.path.join(__BASE_PATH, OUTPUT_FOLDER)
+if not os.path.exists(__FULL_OUTPUT_FOLDER):
+    os.makedirs(__FULL_OUTPUT_FOLDER)
+
+# %% Prepare Datasets
+train_df = pd.read_csv(os.path.join(__BASE_PATH, INPUT_FOLDER_PATH, TRAIN_FILENAME))
+test_df = pd.read_csv(os.path.join(__BASE_PATH, INPUT_FOLDER_PATH, TEST_FILENAME))
+
+train_df.info()
+
+# %% Preprocess csv to txt for fasttext input
+fasttext_preprocessed_train = train_df[['text_a', 'label']]
+fasttext_preprocessed_test = test_df[['text_a', 'label']]
+
+fasttext_preprocessed_train.iloc[:, 0] = fasttext_preprocessed_train.iloc[:, 0].apply(lambda x: ' '.join(simple_preprocess(x)))
+fasttext_preprocessed_test.iloc[:, 0] = fasttext_preprocessed_test.iloc[:, 0].apply(lambda x: ' '.join(simple_preprocess(x)))
+
+fasttext_preprocessed_train.iloc[:, 1] = fasttext_preprocessed_train.iloc[:, 1].apply(lambda x: ''.join('__label__' + x))
+fasttext_preprocessed_test.iloc[:, 1] = fasttext_preprocessed_test.iloc[:, 1].apply(lambda x: ''.join('__label__' + x))
+
+fasttext_preprocessed_train[['label', 'text_a']].to_csv(
+    os.path.join(__BASE_PATH, OUTPUT_FOLDER, FASTTEXT_TRAIN_FILENAME),
+    index = False,
+    sep = ' ',
+    header = None,
+    quoting = csv.QUOTE_NONE, 
+    quotechar = "", 
+    escapechar = " "
+)
+
+fasttext_preprocessed_test[['label', 'text_a']].to_csv(
+    os.path.join(__BASE_PATH, OUTPUT_FOLDER, FASTTEXT_TEST_FILENAME),
+    index = False,
+    sep = ' ',
+    header = None,
+    quoting = csv.QUOTE_NONE, 
+    quotechar = "", 
+    escapechar = " "
+)
+
+# %% Deep learning: fasttext
+# https://fasttext.cc/docs/en/python-module.html
+
+# Train
+dl_model = fasttext.train_supervised(os.path.join(__BASE_PATH, OUTPUT_FOLDER, FASTTEXT_TRAIN_FILENAME))
+# %% Test
+dl_model.test(os.path.join(__BASE_PATH, OUTPUT_FOLDER, FASTTEXT_TEST_FILENAME))
+# %% Predict
+dl_model.predict(TEST_PREDICT_TEXT)
