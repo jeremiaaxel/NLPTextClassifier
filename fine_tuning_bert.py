42e15e0202e125c70377d5807734689e07394598
Josep Marcello <jspmarcello@live.com>
2022-09-24 14:48:45 +0700

    Fix(BERT): add "labels" to tokenized data
    also adjust the code for Google colab

--- fine_tuning_bert.py
+++ fine_tuning_bert.py
@@ -4,13 +4,13 @@ import torch
 import numpy as np
 import evaluate
 import pandas as pd
-from transformers import BertTokenizer, BertForQuestionAnswering, Trainer, TrainingArguments
+from transformers import BertTokenizer, BertForSequenceClassification, Trainer, TrainingArguments
 
 # %% Configurations
 TRAIN_FILENAME = "train.csv"
 TEST_FILENAME = "test.csv"
 LABEL_FILENAME = "labels.txt"
-INPUT_FOLDER_PATH = "data_worthcheck"
+INPUT_FOLDER_PATH = "drive/Shareddrives/NLP/Text Classifier/data_worthcheck"  # For Google colab
 
 FASTTEXT_TRAIN_FILENAME = "train.txt"
 FASTTEXT_TEST_FILENAME = "test.txt"
@@ -25,9 +25,7 @@ if not os.path.exists(__FULL_OUTPUT_FOLDER):
 
 os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:1024'
 
-# BERT_PRETRAINED_MODEL_PATH = "indobenchmark/indobert-base-p2"
-# BERT_PRETRAINED_MODEL_PATH = "bert-base-uncased"
-BERT_PRETRAINED_MODEL_PATH = "bert-base-uncased"
+BERT_PRETRAINED_MODEL_PATH = "indobenchmark/indobert-base-p2"
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 metric = evaluate.load('accuracy')
@@ -50,50 +48,51 @@ test_labels = list(test_df['label'])
 # %% Encode training data (create input IDs, token type IDs, and attention masks) in the form of tenors
 tokenizer = BertTokenizer.from_pretrained(BERT_PRETRAINED_MODEL_PATH, do_lower_case=True)
 
-train_encodings = [
-	tokenizer(query, truncation=True, padding='max_length', max_length=280, return_tensors='pt') for
-	(i, query) in enumerate(train_queries)]
-# Squeeze the tensors into a 1D array
-train_encodings = [{keys: encoding[keys].squeeze(0) for keys in encoding} for encoding in train_encodings]
 
-test_encodings = [
-	tokenizer(query, truncation=True, padding='max_length', max_length=280, return_tensors='pt') for
-	(i, query) in enumerate(test_queries)]
-test_encodings = [{keys: encoding[keys].squeeze(0) for keys in encoding} for encoding in test_encodings]
+def tokenize(queries):
+	tokenized_queries = [
+		{**tokenizer(query, truncation=True, padding='max_length', max_length=280, return_tensors='pt'),
+		 'labels': 0 if train_labels[i] == 'no' else 1} for (i, query) in enumerate(queries)]
+	# Squeeze the tensors into a 1D tensor
+	return [{key: toked[key].squeeze(0) if torch.is_tensor(toked[key]) else toked[key] for key in toked} for toked in
+			tokenized_queries]
 
-train_encodings[0]
+
+tokenized_train_data = tokenize(train_queries)
+tokenized_test_data = tokenize(test_queries)
+
+tokenized_train_data[0]
 
 # %% Create model
-model = BertForQuestionAnswering.from_pretrained(BERT_PRETRAINED_MODEL_PATH, num_labels=2)
+model = BertForSequenceClassification.from_pretrained(BERT_PRETRAINED_MODEL_PATH, num_labels=2)
 model.to(device)
 
 # %% fine-tune model
+torch.cuda.empty_cache()
+
+
 def compute_metrics(eval_pred):
 	logits, labels = eval_pred
 	predictions = np.argmax(logits, axis=-1)
 	return metric.compute(predictions=predictions, references=labels)
 
 
-torch.cuda.empty_cache()
 training_args = TrainingArguments(
-	# num_train_epochs=10,
-	# weight_decay=0.01,
-	# load_best_model_at_end=True,
-	# logging_steps=200,
-	# save_steps=400,
-	# evaluation_strategy='steps',
 	evaluation_strategy='epoch',
-	per_device_train_batch_size=4,
+	logging_steps=200,
+	per_device_train_batch_size=16,
 	output_dir='./bert-output',
+	weight_decay=0.01,
 )
 trainer = Trainer(
 	model=model,
 	args=training_args,
-	train_dataset=train_encodings,
-	eval_dataset=test_encodings,
+	train_dataset=tokenized_train_data,
+	eval_dataset=tokenized_test_data,
 	compute_metrics=compute_metrics,
+	tokenizer=tokenizer,
 )
 trainer.train()
 
 # %%
-model(**test_encodings[0])
+model(**tokenized_test_data[0])
